74048982e0fb5ba6495f4579fdd6ac3fc1ada93f
Paul van der Walt <paul@denknerd.org>
2015-03-03 13:32:58 +0100

    options roughly working :/

--- src/Main.hs
+++ src/Main.hs
@@ -12,20 +12,38 @@ import UU.PPrint (putDoc)
 
 import System.Console.CmdArgs
 
-data Backends = PPDiaspec
-              | RacketSpec
-              | JavaFW
-                deriving (Show, Eq, Data, Typeable)
+data DiaspecCompiler = Pretty { inFile :: FilePath
+                              , outFile :: FilePath}
+                     | Java   { inFile :: FilePath
+                              , outDir :: FilePath}
+                     | Racket { inFile :: FilePath
+                              , outFile :: FilePath}
+                     deriving (Show, Data, Typeable)
 
-data CmdOptions = CmdOptions { inputFile :: String
-                             , mode      :: Backends}
-                  deriving (Show, Data, Typeable)
+-- outFlags x = x &= help "Destination directory" &= typDir
 
-defaultOpts = CmdOptions { inputFile = def
-                         , mode = PPDiaspec}
+pretty = Pretty { inFile = def &= argPos 0 &= typFile  -- &= help "Specification file to process."
+                , outFile = outFileHelp} &= auto
+java   = Java   { inFile = def &= argPos 0 &= typFile  -- &= help "Specification file to process."
+                , outDir = def &= argPos 1  &= typDir}
+racket = Racket { inFile = def &= argPos 0 &= typFile  -- &= help "Specification file to process."
+                , outFile = outFileHelp}
+  
+outFileHelp = "-" &= help "Output file. Default to STDOUT." &= typFile
 
 main :: IO ()
 main = do
-  cmdArgs defaultOpts
-  txt <- getContents
-  (putDoc . prettyDia . parseGrammar . alexScanTokens) txt
+  opt <- cmdArgs $ modes [pretty, racket, java] &= help "bluh?" &= program "diaspec"
+  case opt of
+   (Pretty infile out) -> handlePretty infile out
+   (_)          -> undefined
+
+handlePretty :: FilePath -> FilePath -> IO ()
+handlePretty i o = do
+  putStrLn "Pretty-print mode selected."
+  spec <- case i of
+           "-" -> do putStrLn "Reading from STDIN."
+                     getContents
+           _   -> do putStrLn ("Dealing with file: " ++ show i)
+                     readFile i
+  (putDoc . prettyDia . parseGrammar . alexScanTokens) spec
