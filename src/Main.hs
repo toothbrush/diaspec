8afb5bf1c285ecea829feb688823cc061150cffe
Paul van der Walt <paul@denknerd.org>
2015-03-03 12:30:12 +0100

    Start working on Getopts

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 -- Copyright 2015 © Paul van der Walt <paul.vanderwalt@inria.fr>
---
+
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
 import Diaspec.Frontend.Parser (parseGrammar)
@@ -8,4 +10,22 @@ import Diaspec.Backend.PrintDiaspec
 
 import UU.PPrint (putDoc)
 
-main = getContents >>= putDoc . prettyDia . parseGrammar . alexScanTokens
+import System.Console.CmdArgs
+
+data Backends = PPDiaspec
+              | RacketSpec
+              | JavaFW
+                deriving (Show, Eq, Data, Typeable)
+
+data CmdOptions = CmdOptions { inputFile :: String
+                             , mode      :: Backends}
+                  deriving (Show, Data, Typeable)
+
+defaultOpts = CmdOptions { inputFile = def
+                         , mode = PPDiaspec}
+
+main :: IO ()
+main = do
+  cmdArgs defaultOpts
+  txt <- getContents
+  (putDoc . prettyDia . parseGrammar . alexScanTokens) txt
