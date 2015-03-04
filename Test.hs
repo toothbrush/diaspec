21732f2e60684684ef42133147e8d69bb63db9e9
Paul van der Walt <paul@denknerd.org>
2015-03-04 18:05:37 +0100

    make a cmdline param to specify java file to give AST of.

--- Test.hs
+++ Test.hs
@@ -1,14 +1,28 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
 import Language.Java.Parser
 import Language.Java.Syntax
 import Language.Java.Pretty
 
+import System.Console.CmdArgs
 
-main = do ast <- parser compilationUnit `fmap` readFile "AbstractCamera.java"
-          case ast of
-             Left a -> error "die"
-             Right pr ->
-               do  putStrLn $ prettyPrint pr
-                   putStrLn "\n\nBecomes:\n\n"
-                   putStrLn $ show pr
+  
+data Test = Test { inFile :: FilePath }
+                     deriving (Show, Data, Typeable)
+
+test = Test { inFile = def &= args &= typFile }
+
+
+
+main = do
+  opt <- cmdArgs test
+  case opt of
+   Test i -> do
+      ast <- parser compilationUnit `fmap` readFile i
+      case ast of
+         Left a -> error "die"
+         Right pr ->
+           do  putStrLn $ prettyPrint pr
+               putStrLn "\n\nBecomes:\n\n"
+               putStrLn $ show pr
