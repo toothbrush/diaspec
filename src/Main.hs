d33e763059b1b217e97089e6baff8410466e07ab
Paul van der Walt <paul@denknerd.org>
2015-03-05 21:57:20 +0100

    super fragile options parser seems correct now.

--- src/Main.hs
+++ src/Main.hs
@@ -20,71 +20,91 @@ import Data.List.Utils (split)
 import Data.Char (isAlpha)
 import System.Console.CmdArgs
 import System.FilePath
+import System.Environment (getArgs, withArgs)
 
-data DiaspecCompiler = Pretty { inFile :: FilePath
-                              , outFile :: Maybe FilePath}
-                     | Racket { inFile :: FilePath
-                              , outFile :: Maybe FilePath}
-                     | Java   { inFile :: FilePath }
-                             -- , outDir :: FilePath}
-                     deriving (Show, Data, Typeable)
+data DSOptions = Pretty { inFile :: FilePath
+                        , output :: Maybe FilePath}
+               | Racket { inFile :: FilePath
+                        , output :: Maybe FilePath}
+               | Java   { inFile :: FilePath }
+                       -- , outDir :: FilePath}
+               deriving (Show, Data, Typeable)
 
-pretty = Pretty { inFile = def &= argPos 0 &= typFile
-                , outFile = Nothing &= typFile} -- &= explicit &= name "ppspec"
-         -- &= auto
-java   = Java   { inFile = def &= argPos 0 &= typFile
-                           } -- &= explicit &= name "java"
-                -- , outDir = def &= argPos 1 &= typDir}
-racket = Racket { inFile = def &= argPos 0 &= typFile
-                , outFile = Nothing &= typFile} -- &= explicit &= name "racket"
+typSpec = typ "SPEC"
 
--- outFileHelp = Nothing &= help "Output file. Default to STDOUT." &= typFile
+pretty = Pretty { inFile = def &= argPos 0 &= typSpec
+                , output = Nothing &= outhelp &= typFile
+                } &= help "Neaten up a Diaspec file."
+--                  &= details  [ "Examples:"
+--                              , "Blah blah blah."
+--                              ]
+java   = Java   { inFile  = def &= argPos 0 &= typSpec
+                -- , outDir = def &= argPos 1 &= typDir
+                } &= auto
+                  &= help "Generate Java framework from spec. (default mode)"
+racket = Racket { inFile = def &= argPos 0 &= typSpec
+                , output = Nothing &= outhelp &= typFile
+                } &= help "Convert Diaspec into Racket spec."
 
-mode = cmdArgsMode $ modes [racket, java, pretty] &= program "diaspec" -- &= help "bluh?"
+outhelp = help "Write to FILE. Default = STDOUT."
+
+mode = cmdArgsMode $
+       modes [java, pretty, racket]
+       &= program "diaspec"
+       &= help "Compile Diaspec specifications to various useful formats."
+       &= helpArg [explicit, name "help", name "h"]
+
+getOpts :: IO DSOptions
+getOpts = cmdArgsRun mode
 
 main :: IO ()
 main = do
-  opt <- cmdArgsRun mode
-  case opt of
-   (Pretty infile out) ->
-     do
+  args <- getArgs
+  -- If the user did not specify any arguments, pretend "--help" was given
+  opts <- (if null args then withArgs ["--help"] else id) getOpts
+  optionHandler opts
+
+optionHandler :: DSOptions -> IO ()
+optionHandler (Pretty infile out) = do
        putStrLn "[DIASPEC] Pretty-print mode selected."
        handlePretty prettyDia    infile out
-   (Racket infile out) ->
-     do
+optionHandler (Racket infile out) = do
        putStrLn "[RACKET] Pretty-print mode selected."
        handlePretty prettyRacket infile out
-   (Java infile)->
-   --(Java infile outdir)->
-     do
+optionHandler (Java infile)       = do
+            --(Java infile outdir)->
        putStrLn "[JAVA] Generate framework."
        handleJava infile ""
 
+readSomething :: FilePath -> IO (String, String)
+readSomething "-" = do putStrLn "Reading from STDIN."
+                       spec <- getContents
+                       let pn  = "stdin"
+                       return (pn, spec)
+readSomething i   = do putStrLn$ "Reading from file: " ++ show i
+                       putStr "\n\n----\n\n"
+                       spec <- readFile i
+                       let pn  = filter isAlpha $ takeBaseName i
+                       return (pn, spec)
+
+writeStng :: Maybe FilePath -> String -> IO ()
+writeStng Nothing _    = return ()
+writeStng (Just "-") _ = return () -- we already write to STDOUT
+writeStng (Just f)   c = do putStrLn$"----\n\nWriting to file: " ++ show f ++ "."
+                            writeFile f c
+
 -- TODO less fugly method of building Specification from [Declaration] :/
-handleJava :: FilePath -> FilePath -> IO()
-handleJava i o = do
-  spec <- case i of
-           "-" -> do putStrLn "Reading from STDIN."
-                     getContents
-           _   -> do putStrLn ("Dealing with file: " ++ show i)
-                     putStr "\n\n----\n\n"
-                     readFile i
-  let pn  = filter isAlpha $ takeBaseName i
-  let res = ((genJava . S pn) . parseGrammar . alexScanTokens) spec
-  mapM_ (putStrLn . prettyPrint) res
+handleJava :: FilePath -> FilePath -> IO ()
+handleJava i   o = do (pn, spec) <- readSomething i
+                      let res = ((genJava . S pn) . parseGrammar . alexScanTokens) spec
+                          txt = map prettyPrint res
+                      --TODO write files into DIR
+                      mapM_ putStrLn txt
+                      
 
 handlePretty :: (Specification -> PP_Doc) -> FilePath -> Maybe FilePath -> IO ()
 handlePretty pf i o = do
-  spec <- case i of
-           "-" -> do putStrLn "Reading from STDIN."
-                     getContents
-           _   -> do putStrLn ("Dealing with file: " ++ show i)
-                     readFile i
-  let pn  = filter isAlpha $ takeBaseName i
+  (pn,spec) <- readSomething i
   let res = ((pf . S pn) . parseGrammar . alexScanTokens) spec
   render res 80
-  case o of
-    Nothing -> return ()
-    Just fname -> do
-      writeFile fname (disp res 80 "")
-      putStrLn $ "---\n\nWriting to file " ++ show fname ++ "."
+  writeStng o (disp res 80 "")
