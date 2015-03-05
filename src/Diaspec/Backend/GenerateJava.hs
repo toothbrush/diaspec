e3c39c70cb71d21461329b9479f8625e9619efc5
Paul van der Walt <paul@denknerd.org>
2015-03-05 15:59:34 +0100

    deal with TyEnvironments better.

--- src/Diaspec/Backend/GenerateJava.hs
+++ src/Diaspec/Backend/GenerateJava.hs
@@ -10,13 +10,12 @@ module Diaspec.Backend.GenerateJava where
 -- Copyright 2015 © Paul van der Walt <paul.vanderwalt@inria.fr>
 
 import Diaspec.Backend.AG
-import Diaspec.Sort ()
-import Data.List (sort)
 import Language.Java.Syntax
 
 -- give this a name for exporting.
 genJava :: Specification -> [CompilationUnit] -- CompilationUnit roughly corresponds to a Java file.
-genJava s = genJ_Syn_Specification (wrap_Specification (sem_Specification s)
-                                                        (Inh_Specification{}))
+genJava (S pn s) = genJ_Syn_Specification (wrap_Specification
+                                           (sem_Specification(S pn$ s))
+                   (Inh_Specification{}))
 
 
